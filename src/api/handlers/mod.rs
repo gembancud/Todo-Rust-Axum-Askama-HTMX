a5554039be5c588979205b07ffba59c59c8f3de5
Gil Emmanuel Bancud <gembancud@gmail.com>
2024-02-19 14:43:35 +0800

    Extract todo into different module

--- src/api/handlers/mod.rs
+++ src/api/handlers/mod.rs
@@ -1,81 +1,13 @@
-use std::sync::Arc;
-
+mod todo;
 use crate::api::middleware::htmx_trigger_middleware;
-use askama::Template;
-use axum::{
-    extract::{Path, State},
-    middleware,
-    routing::{delete, get},
-    Form, Router,
-};
+use axum::{middleware, Router};
 
-use tokio::sync::Mutex;
 use tower_http::services::ServeDir;
 
-#[derive(Clone)]
-struct AppState {
-    todos: Arc<Mutex<Vec<String>>>,
-}
-
 pub fn create_router() -> Router {
-    let state = AppState {
-        todos: Arc::new(Mutex::new(vec![
-            String::from("Buy milk"),
-            String::from("Walk the dog"),
-        ])),
-    };
     let router = Router::new()
-        .route("/", get(todo).post(add_todo))
-        .route("/:todo", delete(delete_todo))
-        .route("/clear", get(|| async { TodoField }))
-        .nest_service("/static", ServeDir::new("src/api/templates"));
-
+        .nest("/todo", todo::create_router())
+        .nest_service("/static", ServeDir::new("src/api/templates"))
+        .layer(middleware::from_fn(htmx_trigger_middleware));
     router
-        .with_state(state)
-        .layer(middleware::from_fn(htmx_trigger_middleware))
-}
-
-#[derive(Template)]
-#[template(path = "pages/todos.html")]
-struct HelloTemplate {
-    todos: Vec<String>,
-}
-
-async fn todo(State(state): State<AppState>) -> HelloTemplate {
-    let todos = state.todos.lock().await.clone();
-    HelloTemplate { todos: todos }
-}
-
-#[derive(serde::Deserialize)]
-struct AddTodo {
-    todo: String,
-}
-
-#[derive(Template)]
-#[template(path = "components/todo_list.html")]
-struct TodoListTemplate {
-    todos: Vec<String>,
-}
-
-#[derive(Template)]
-#[template(path = "components/todo_field.html")]
-struct TodoField;
-
-async fn add_todo(State(state): State<AppState>, Form(form): Form<AddTodo>) -> TodoListTemplate {
-    let todo = form.todo;
-    let mut todos = state.todos.lock().await;
-    todos.push(todo);
-
-    TodoListTemplate {
-        todos: todos.clone(),
-    }
-}
-
-async fn delete_todo(State(state): State<AppState>, Path(todo): Path<String>) -> () {
-    let mut todos = state.todos.lock().await;
-    if let Some(index) = todos.iter().position(|t| *t == todo) {
-        todos.remove(index);
-    }
-
-    ()
 }
---
+++ src/api/handlers/todo.rs
@@ -0,0 +1,73 @@
+use std::sync::Arc;
+
+use askama::Template;
+use axum::{
+    extract::{Path, State},
+    routing::{delete, get},
+    Form, Router,
+};
+use tokio::sync::Mutex;
+
+#[derive(Clone)]
+struct AppState {
+    todos: Arc<Mutex<Vec<String>>>,
+}
+pub fn create_router() -> Router {
+    let state = AppState {
+        todos: Arc::new(Mutex::new(vec![
+            String::from("Buy milk"),
+            String::from("Walk the dog"),
+        ])),
+    };
+    let router = Router::new()
+        .route("/", get(todo).post(add_todo))
+        .route("/:todo", delete(delete_todo))
+        .route("/clear", get(|| async { TodoField }));
+
+    router.with_state(state)
+}
+
+#[derive(Template)]
+#[template(path = "pages/todos.html")]
+struct HelloTemplate {
+    todos: Vec<String>,
+}
+
+async fn todo(State(state): State<AppState>) -> HelloTemplate {
+    let todos = state.todos.lock().await.clone();
+    HelloTemplate { todos: todos }
+}
+
+#[derive(serde::Deserialize)]
+struct AddTodo {
+    todo: String,
+}
+
+#[derive(Template)]
+#[template(path = "components/todo_list.html")]
+struct TodoListTemplate {
+    todos: Vec<String>,
+}
+
+#[derive(Template)]
+#[template(path = "components/todo_field.html")]
+struct TodoField;
+
+async fn add_todo(State(state): State<AppState>, Form(form): Form<AddTodo>) -> TodoListTemplate {
+    let todo = form.todo;
+    let mut todos = state.todos.lock().await;
+    todos.push(todo);
+
+    TodoListTemplate {
+        todos: todos.clone(),
+    }
+}
+
+async fn delete_todo(State(state): State<AppState>, Path(todo): Path<String>) -> () {
+    let mut todos = state.todos.lock().await;
+    if let Some(index) = todos.iter().position(|t| *t == todo) {
+        todos.remove(index);
+    }
+
+    ()
+}
